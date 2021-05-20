a9f2503cece0f99411a370ff9d5b524ba1c425d2
drqueijo <silveriomat1234@gmail.com>
2021-05-20 14:32:50 -0300

    Basic Auth

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import Login from './Login'
+import {getTokenFromUrl} from './spotify';
 
 function App() {
+  useEffect (() => {
+    const token = getTokenFromUrl();
+    console.log('TOKEN SUCCESSFUL', token)
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login />
     </div>
   );
 }

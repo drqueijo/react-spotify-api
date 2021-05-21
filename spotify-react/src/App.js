dff2e6d3fc2fc6d1125c3aa9ae0f36b277ba16e0
drqueijo <silveriomat1234@gmail.com>
2021-05-21 00:18:50 -0300

    Player page structure

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -1,16 +1,44 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Login from './Login'
+import Player from './Player'
 import {getTokenFromUrl} from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import { useDataLayerValue} from './DataLayer';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
   useEffect (() => {
-    const token = getTokenFromUrl();
-    console.log('TOKEN SUCCESSFUL', token)
-  })
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token){
+      dispatch({
+        type:'SET_TOKEN',
+        token: _token
+      })
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+
+        dispatch({
+          type:'SET_USER',
+          user: user,
+        })
+      })
+    }
+
+  },[])
+
   return (
     <div className="App">
-      <Login />
+      {
+        token ? <Player spotify={spotify}/> : <Login />        
+      }
     </div>
   );
 }
---
+++ spotify-react/src/reducer.js
@@ -0,0 +1,26 @@
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null,
+    //change token to null after development 
+    token: 'BQDo0m-ZE2Ptp5iF_MnF6oEF_GpuR8s3OFSmH9XOj7i9IsF8-rJraWcnxeNUeSGeDNR6U6BTBmYphMJLJm0yeBtw6se0FDIIfbZhdAge-KQRb6OgkPtR4FPniDKlliAZPPQJh8hkLlw52dVnDUE7HBC5gCOVqOvMkA'
+};
+
+const reducer = (state, action) => {
+    console.log(action);
+    
+    switch (action.type) {
+        case 'SET_USER': return {
+            ...state,
+            user: action.user
+        }
+        case 'SET_TOKEN': return {
+            ...state,
+            token: action.token
+        }
+        default: return state;
+    }
+};
+
+export default reducer;
